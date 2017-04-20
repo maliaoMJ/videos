6f8324dbbed53f8f81b1abd6a5ae41cf320e111f
majian <1302151931@qq.com>
2017-04-20 22:49:06 +0800

    Update toupiao.js

--- js/toupiao.js
+++ js/toupiao.js
@@ -1,3 +1,33 @@
 /**
  * Created by Administrator on 2017/4/20 0020.
  */
+$(function(){
+    //处理同时播放
+    var videoElements=document.getElementsByTagName('video');
+        for(var i=0;i<videoElements.length;i++){
+             videoElements[i].onclick=function() {
+
+                 if (this.paused === true) {
+                     //全部暂停
+                     for (var i = 0; i < videoElements.length; i++) {
+                         videoElements[i].pause();
+                     }
+                     //全部暂停结束
+                     //返回视频的地址
+                     // var videoSrc=this.getElementsByTagName("source")[0].getAttribute('src');
+                     this.play();
+                 } else {
+                     this.pause();
+                 }
+
+
+             }
+
+        }
+    //处理同时播放结束
+
+
+
+
+
+});
